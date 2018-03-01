479f9e34bc3149a163492e4e6fee7786e9048d9f
xw37 <weixiaoyanelsa@gmail.com>
2018-03-01 19:15:05 +0000

    comments

--- src/Hash.java
+++ src/Hash.java
@@ -9,9 +9,13 @@ public class Hash {
             Scanner sc = new Scanner(new File("inputs/a_example.in"));
             Read rd = new Read();
             int[] firstline = rd.scanfirst(sc);
-            Map mp = new Map(firstline[0], firstline[1], firstline[2], firstline[3], firstline[4], firstline[5]);
             ArrayList<Ride> rides = rd.scanRide(sc);
+            // read from file
+
+            Map mp = new Map(firstline[0], firstline[1], firstline[2], firstline[3], firstline[4], firstline[5]);
             mp.setRides(rides);
+            // set the map:)
+
         } catch (FileNotFoundException e) {
             e.getMessage();
         }
