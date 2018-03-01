42e97d4b6c0bfe04f90409307c2c3bf9819a2414
Elsa Wei <weixiaoyanelsa@gmail.com>
2018-03-01 18:55:20 +0000

    Update Intersection.java

--- src/Intersection.java
+++ src/Intersection.java
@@ -6,4 +6,12 @@ public class Intersection {
         this.x = x;
         this.y = y;
     }
+    
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
