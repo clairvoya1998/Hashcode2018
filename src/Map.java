a74ace067bf5fbf53f36962680ed57a40291bfc7
VKuosma <vibe96@gmail.com>
2018-03-01 19:07:01 +0000

    Start of algorithm

--- src/Map.java
+++ src/Map.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Map {
     Vehicle[] vehicles;
     Ride[] rides;
@@ -12,5 +16,40 @@ public class Map {
     }
 
     Map() {
+    }
+
+    void run() {
+        List<Ride> ridesList = Arrays.asList(rides);
+        while (!ridesList.isEmpty()) {
+            Ride ride = ridesList.get(0);
+            //Sort vehicles based on position
+
+            boolean needsCar = true;
+            int i = 0;
+            while (needsCar) {
+                Vehicle vehicle = vehicles.get(i);
+
+                if (canMakeTheRide) {
+                    //Set new location to vehicle.
+                    if (carwillbeearly) {
+                        vehicle.currentStep = ride.earliest + duration;
+                    }
+                    else
+                        vehicle.currentStep += timeToStartLocation + duration;
+                    needsCar = false;
+                    vehicle.rides.add(ride);
+                    ridesList.remove(0);
+                }
+                else {
+                    if (i >= ridesList.size()) {
+                        //No car that can make the trip was found, break from loop and abandon ride
+                        ridesList.remove(0);
+                        break;
+                    }
+                    i++;
+                }
+            }
+
+        }
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
 public class Vehicle {
+
+    ArrayList<Ride> rides;
+    Intersection location;
+    int currentStep;
 
 }
